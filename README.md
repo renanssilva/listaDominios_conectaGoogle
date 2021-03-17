3e973ff4b28a28a934daa7863e4d76dbb24bf370
RenanSoares <renan.silva.soares.br@gmail.com>
2021-03-17 12:05:55 -0300

    ajustes no código e add-->README

---
+++ README.md
@@ -0,0 +1,2 @@
+# ListaDominios_conectaGoogle
+O ListaDominios_conectaGoogle lista e agrupa seus contatos da sua conta google pelo dominio
--- app.py
+++ app.py
@@ -18,54 +18,29 @@ print(STORAGE)
 
 def authorize_credentials():
     # Fetch credentials from storage
-    print('1')
     credentials = STORAGE.get()
-    print('2')
     # If the credentials doesn't exist in the storage location then run the flow
     if credentials is None or credentials.invalid:
-        print('3')
         flow = flow_from_clientsecrets(CLIENT_SECRET, scope=SCOPE)
-        print('4')
         http = httplib2.Http()
-        print('5')
         credentials = run_flow(flow, STORAGE, http=http)
-        print('6')
-    print('7')
     return credentials
 
 def get_google_contacts():
     credentials = authorize_credentials()
-    print("credentials", credentials)
     http = credentials.authorize(httplib2.Http())
-    print('htto', http)
     discoveryUrl = ('https://people.googleapis.com/$discovery/rest?version=v1')
     service = discovery.build('people', 'v1', http=http, discoveryServiceUrl=discoveryUrl)
-    # print("service", service)
-    # service = discovery.build('people', 'v1', http=http)
-
     results = service.otherContacts().list(readMask='emailAddresses', pageSize=1000).execute()
-    print('results', results)
-    print(results['otherContacts'])
-    print(results['otherContacts'][0])
-    # values = results['otherContacts']
     values = results['otherContacts']
-    # [50]['emailAddresses'][0]['value']
-    print(len(values))
     list_emails = []
     for email in values:
         list_emails.append(email['emailAddresses'][0]['value'])
-
-    print(type(values))
-
     return sorted(list_emails)
-
-# credentials = authorize_credentials()
 
 def logout_goole():
     credentials = STORAGE.delete()
     return credentials
-
-# authorize_credentials()
 
 # Default route
 @app.route('/')
@@ -76,29 +51,23 @@ def index():
 # Google login route
 @app.route('/login')
 def google_login():
-    # redirect_uri = url_for('google_authorize', _external=True)
     credentials = None
 
     if not credentials:
-        print("Certinho")
-        credentials = authorize_credentials()
-        print(credentials)
+        authorize_credentials()
     return render_template('logado.html', enable=True)
 
 
 # Google authorize route
-@app.route('/contatos')
-def google_authorize(*args):
-    emailAddresses = get_google_contacts()
-    return f"{emailAddresses}"
-
-
+# @app.route('/contatos')
+# def google_authorize(*args):
+#     emailAddresses = get_google_contacts()
+#     return f"{emailAddresses}"
 
 @app.route('/logout')
 def logout():
-    for key in list(session.keys()):
-        session.pop(key)
-    return redirect('/')
+    pass
+    return 'logout'
 
 
 if __name__ == '__main__':
