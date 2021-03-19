2641d8225dcace95f316f9925fd955e83bbb544f
RenanSoares <renan.silva.soares.br@gmail.com>
2021-03-19 15:16:31 -0300

    Ajustes finais

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-
-# from flask import Flask, render_template, url_for, redirect, session
 from flask import Flask, render_template
 import httplib2
 from googleapiclient import discovery
-
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run_flow
@@ -11,23 +8,30 @@ from oauth2client.tools import run_flow
 
 app = Flask(__name__)
 
-CLIENT_SECRET = 'credentials.json'
+CREDENTIAL_JSON = 'credentials.json'
 SCOPE = 'https://www.googleapis.com/auth/contacts.other.readonly'
 STORAGE = Storage('conecta-credentials-storage')
 
 
 def authorize_credentials():
-    # Fetch credentials from storage
+    """
+    Buscando credenciais no STORAGE ou autenticar credenciais
+    :return: credencial autorizada
+    """
     credentials = STORAGE.get()
     # If the credentials doesn't exist in the storage location then run the flow
     if credentials is None or credentials.invalid:
-        flow = flow_from_clientsecrets(CLIENT_SECRET, scope=SCOPE)
+        flow = flow_from_clientsecrets(CREDENTIAL_JSON, scope=SCOPE)
         http = httplib2.Http()
         credentials = run_flow(flow, STORAGE, http=http)
     return credentials
 
 
 def get_google_contacts():
+    """
+    Pega todos os contatos do usuário autenticado
+    :return:Uma lista com todos os contatos do usuário autenticado
+    """
     credentials = authorize_credentials()
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ('https://people.googleapis.com/$discovery/rest?version=v1')
@@ -39,11 +43,15 @@ def get_google_contacts():
 
     for email in values:
         values_list.append(email['emailAddresses'][0]['value'])
-
     return values_list
 
 
 def format_googleContacts(contactList: list):
+    """
+    Formata a lista de contatos do usuário autenticado
+
+    :return: Um dicionário ourdenado sendo as chaves od dominios e os valores todos os email relacionado a este dominio
+    """
     dictContact = {}
 
     for addressesEmail in contactList:
@@ -61,27 +69,23 @@ def format_googleContacts(contactList: list):
     return dict(orderDictContact)
 
 
-# authorize_credentials()
+# Definindo as rotas
 
-# Default route
+# Rota padrão
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-# Google login route
+# Rota de login do google
 @app.route('/login')
 def google_login():
-
     authorize_credentials()
-
     contacts = get_google_contacts()
-
     formatContacts = format_googleContacts(contacts)
-
     return render_template('logado.html', enable=True, contacts=formatContacts)
 
-
+# Rota de logout
 @app.route('/logout')
 def logout_goole():
     STORAGE.delete()
